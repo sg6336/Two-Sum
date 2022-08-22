80656dd187aac6fd67b5b074a47cea42fcb08429
Denys <navrotskyidenys@gmail.com>
2022-08-23 01:17:17 +0300

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.TwoSum(new int[] { 1, 2, 3 }, 4); // two_sum([1, 2, 3], 4) == {0, 2}
+        }
+    }
+
+    public class Kata
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int sum = 0;
+            int[] ints = new int[2];
+
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                sum = numbers[j];
+
+                if (j+1 < numbers.Length)
+                {
+                    for (int i = j + 1; i < numbers.Length; i++)
+                    {
+                        sum += numbers[i];
+
+                        if (sum == target)
+                        {
+                            ints[0] = j;
+                            ints[1] = i;
+                            break;
+                        }
+
+                        sum -= numbers[i];
+                    }
+
+                    if (sum == target)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Not found");
+                }
+
+            }
+            return ints;
+        }
+    }
+}
+
+
---
+++ Two Sum/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace Two_Sum
+{
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+    using ConsoleApp1;
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new[] { 0, 2 }, Kata.TwoSum(new[] { 1, 2, 3 }, 4).OrderBy(a => a).ToArray());
+            Assert.AreEqual(new[] { 1, 2 }, Kata.TwoSum(new[] { 1234, 5678, 9012 }, 14690).OrderBy(a => a).ToArray());
+            Assert.AreEqual(new[] { 0, 1 }, Kata.TwoSum(new[] { 2, 2, 3 }, 4).OrderBy(a => a).ToArray());
+        }
+    }
+}
